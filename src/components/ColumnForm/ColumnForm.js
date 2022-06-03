a290614dd877592fd959bf64ab78f3161f4a973f
Monika Swoboda <mon.swoboda@gmail.com>
2022-06-03 15:08:38 +0200

    11.6.3 - Add icon to new column

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,17 +2,20 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 
 const ColumnForm = props => {
-        const [value, setValue] = useState(''); //dodaje do komponentu nowy stan - useState, o wartosci pustego stringa, dostep do wartosci tego stanu bedzie dostepny pod stala value, a funckja do modyfikacji pod stala setValue
-        
+        const [title, setTitle] = useState(''); //dodaje do komponentu nowy stan - useState, o wartosci pustego stringa, dostep do wartosci tego stanu bedzie dostepny pod stala value, a funckja do modyfikacji pod stala setValue
+        const [icon, setIcon ] = useState('');
+
         const handleSubmit = e => {
             e.preventDefault();
-            props.action({ title: value });
-            setValue('');
+            props.action({ title: title, icon: icon });
+            setTitle('');
+            setIcon('');
         }
 
         return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+           Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+           Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
             <button>Add column</button>
         </form>
     );
