74193cf9eb303a82f5b70f7dc5584b3c50104d2b
Monika Swoboda <mon.swoboda@gmail.com>
2022-06-15 22:20:41 +0200

    14.1.2-5 Add new pages

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const About = () => {
+    return (
+        <section>
+            <PageTitle>About</PageTitle>
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Suspendisse commodo orci vitae enim ullamcorper pharetra. 
+                Fusce consequat, lectus quis sollicitudin vulputate, nunc sem mattis nibh, sed ultrices lectus metus ut metus. 
+                Morbi aliquet congue sodales. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Donec iaculis est ac tempus interdum. Suspendisse viverra auctor lectus at consequat. Aliquam eu elementum eros, a aliquam nulla.
+            </div>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,15 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const Favorite = () => {
+    return (
+        <section>
+            <PageTitle>Favorite</PageTitle>
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Suspendisse commodo orci vitae enim ullamcorper pharetra. 
+            </div>
+        </section>
+    )
+}
+
+export default Favorite;
