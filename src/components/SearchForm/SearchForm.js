32c12c95c02e2e6121d679cb9222bc3a2679d799
Monika Swoboda <mon.swoboda@gmail.com>
2022-06-15 13:47:40 +0200

    13.6 Implement selectors and action creators

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,6 +3,7 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { updateSearching } from '../../redux/store';
 
 const SearchForm = () => {
     const [searchString, setSearchString] = useState('');
@@ -10,9 +11,8 @@ const SearchForm = () => {
     
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch({type: 'UPDATE_SEARCHSTRING', payload: { searchString } });
+        dispatch(updateSearching({ searchString }));
         setSearchString('');
-        console.log(searchString);
     };
     
     return (
---
+++ src/utils/strContains.js
@@ -0,0 +1,7 @@
+const strContans = (firstStr, secondStr) => {
+    if (firstStr.toLowerCase().includes(secondStr.toLowerCase()))
+        return true
+        else
+        return false
+}
+  export default strContans;
