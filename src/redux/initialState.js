fcff0ddbdedf48ebf3402faad362328299f71f31
Monika Swoboda <mon.swoboda@gmail.com>
2022-06-20 12:57:45 +0200

    14.3.4 Modify lists and links

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -40,12 +40,12 @@ const initialState = {
 
     lists: [
       {
-        id: 1,
+        id: '1',
         title: 'Things to do...',
         description: 'Interesting things I want to check out'
       },
      {
-        id: 2,
+        id: '2',
         title: 'Test list',
         description: 'Lorem Ipsum'
      }
